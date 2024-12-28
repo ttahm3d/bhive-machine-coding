a0af73f36174ca6fb70daa1501fc2efa5fc8faa7
ttahm3d <ttahm3d@gmail.com>
2024-12-28 13:35:25 +0530

    feat -revert changes

--- src/pages/home/hero.tsx
+++ src/pages/home/hero.tsx
@@ -4,7 +4,7 @@ import Typography from "@/components/ui/typography";
 export default function Hero() {
   return (
     <section>
-      <div className="grid grid-cols-12 px-5 md:px-[120px] bg-[url(./assets/background-vector-desktop.svg)] h-96 container bg-no-repeat bg-right md:bg-center  bg-cover">
+      <div className="grid grid-cols-12 px-5 md:px-[120px] bg-[url(./assets/background-vector-desktop.svg)] h-96 bg-no-repeat bg-right md:bg-center  bg-cover">
         <div className="col-span-12 md:col-span-8 self-center order-2">
           <Typography
             variant="h1"
