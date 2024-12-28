9bfc13be23c2e236f46f0a94ef434e0a642a06d0
ttahm3d <ttahm3d@gmail.com>
2024-12-28 13:14:28 +0530

    feat - made hero section responsive

--- src/pages/home/hero.tsx
+++ src/pages/home/hero.tsx
@@ -4,16 +4,22 @@ import Typography from "@/components/ui/typography";
 export default function Hero() {
   return (
     <section className="">
-      <div className="grid grid-cols-12 px-5 md:px-[120px] bg-[url(./assets/background-vector-desktop.svg)] h-96 container bg-no-repeat bg-center bg-cover">
-        <div className="col-span-8 self-center">
+      <div className="grid grid-cols-12 px-5 md:px-[120px] bg-[url(./assets/background-vector-desktop.svg)] h-96 container bg-no-repeat bg-right md:bg-center  bg-cover">
+        <div className="col-span-12 md:col-span-8 self-center order-2">
           <Typography
             variant="h1"
-            className="text-brand-primary tracking-[-1px]">
+            className="text-brand-primary tracking-[-1px] hidden md:block">
+            Host your meeting with world-class amenities. Starting at{" "}
+            <span className="text-brand-yellow">₹199/-!</span>
+          </Typography>
+          <Typography
+            variant="h4"
+            className="text-brand-primary tracking-[-1px] block md:hidden">
             Host your meeting with world-class amenities. Starting at{" "}
             <span className="text-brand-yellow">₹199/-!</span>
           </Typography>
         </div>
-        <div className="col-span-4 pt-1">
+        <div className="col-span-12 md:col-span-4 pt-1">
           <video src={HeroVideo} autoPlay></video>
         </div>
       </div>
